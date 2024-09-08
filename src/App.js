8bdbc79ad24aae49f0fdb49343da9813e58fdcb2
Yevgine <ev.shahinyan@gmail.com>
2024-09-09 02:42:04 +0400

    Added About section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
 import Header from './components/Header/Header.jsx';
+import About from './components/About/About.jsx';
 import Hero from './components/Hero/Hero.jsx';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero />
+      <About />
     </div>
   );
 }
---
+++ src/components/About/About.jsx
@@ -0,0 +1,24 @@
+import './About.css'
+
+function About(){
+    return (
+        <section className="about">
+            <div className="container">
+                <div className="section-title">
+                    <h2>Մեր <span>մասին</span></h2>
+                    <p>Ընկերության գործունեության մասին</p>
+                </div>
+                <div className="section-box">
+                    <div className="section-info">
+                        <p>SmartCode ծրագրավորման ուսուցման ընկերությունը արդեն 8 տարի է կազմակերպում և իրականացնում է ծրագրավորման դասընթացներ, նպատակ ունենալով աշխատաշուկային հանձնել որակյալ մասնագետներ:</p>
+                        <p>Հետևելով համաշխարհային IT թրենդներին՝ թարմացվում և կատարելագործվում են դասավանդման ծրագրերը: Հատուկ մշակված ծրագրի արդյունքում մեր ուսանողները գրանցում են մեծ հաջողություններ: Հմուտ մասնագետների աշխատանքի արդյունքում մենք վայելում ենք մեր ուսանողների սերն ու վստահությունը:</p>
+                        <p>Դու էլ միացի՛ր մեզ, դարձի՛ր մրցունակ ծրագրավորման ոլորտում SmartCode-ի օգնությամբ:</p>
+                    </div>
+                    <img src="https://smartcode.am/public/image/per3.svg" className="section-image" alt='About' />
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default About;
